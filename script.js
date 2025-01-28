26bf28ff7916ec6e0ed4f3af6e187dcf7e365735
jamesallvs978 <jamesalves9978@gmail.com>
2025-01-28 13:38:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const buttons = document.querySelectorAll('.btn');
-    
-    buttons.forEach(button => {
-        button.addEventListener('click', (e) => {
-            alert("Você será redirecionado para a página de compra do produto.");
-        });
-    });
-});
+console.log("Página carregada com sucesso!");
